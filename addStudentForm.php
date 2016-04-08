d1f4c12087744556d3c3d82a583dffb0eb90412f
ammarjamal <ammar96taie@gmail.com>
2016-04-08 17:01:52 +0400

    Added files via upload

---
+++ addStudentForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form id="stAdder" name ="stAdder" method="post" action="addStudent.php" >
+Student ID: <input type="text" name="SID" id="SID">
+Student Name: <input type="text" name="name" id="name">
+Address: <input type="text" name="address" id="address">
+Year: <input type="text" name="year" id="year">
+Course ID: <input type="text" name="CID" id="CID">
+<input type="submit" name="add" id="add" value="add">
+</form>
+</body>
+</html>
