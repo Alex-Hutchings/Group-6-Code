b3b7712ca93744fb165da1f61f2154cee7b43277
Alex-Hutchings <hutchalex@live.co.uk>
2016-04-13 19:12:46 +0100

    Update addStudent.php
    Added Comments

--- addStudent.php
+++ addStudent.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 <?php
+//Checks the post array for the following variables. If they have been set then 
+//they are assigned to instance variables, for use on this form.
 if(isset($_POST['SID'])){
 $sid = $_POST['SID'];
 }
@@ -24,7 +26,7 @@ $year = $_POST['year'];
 if(isset($_POST['CID'])){
 $cid = $_POST['CID'];
 }
-
+//establishes connection to the database
 $server = "csmysql.cs.cf.ac.uk";
 $user = "group6.2015"; 
 $password = "bhF54FWzyq"; 
@@ -34,7 +36,7 @@ if( $db === FALSE ){
 header( "Location: error.html" ); die();
 }
 $update = null;
-
+//if there was a problem with the connection to the database an error is displayed telling the user
 if ($db->connect_error) {
 die("Connection failed: " . $db->connect_error); }
 if(isset($_SESSION['id'])){
@@ -43,7 +45,8 @@ $update = $_SESSION['id'];
 // sql command to update a record
 $sql = "INSERT INTO STUDENT (Student_ID, Student_NAME, Address, Study_YEAR, Course_ID) VALUES ('".$sid."','".$name."','".$address."','".$year."','".$cid."')";
 
-
+//Section that checks whether the query was completed successfully, if it was then a message is displayed saying so,
+//if it was unsuccessful then it displays that message
 if ($db->query($sql) === TRUE) {
 $message = "Record added successfully"; echo $message;
 } else {
