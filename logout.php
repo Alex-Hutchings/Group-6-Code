3ffe364ec866b95e8c3a90bf4c41e6278bf5d811
Alex-Hutchings <hutchalex@live.co.uk>
2016-04-13 19:13:43 +0100

    Update logout.php
    Added Comments

--- logout.php
+++ logout.php
@@ -1,4 +1,6 @@
 <?php
+//code for a logout button. Destroys the current session removing any session variables from the session array
+//redirects users to the login page so they must login once again
 session_destroy();
 header('location:login-2.php');
 ?>
