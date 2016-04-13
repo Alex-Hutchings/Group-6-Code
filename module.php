469e2de136abd976a58887cd1e89a41c658dfd20
Alex-Hutchings <hutchalex@live.co.uk>
2016-04-13 20:22:19 +0100

    Update module.php
    commented

--- module.php
+++ module.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php 
-session_start();
-?>
+<?php session_start();?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,14 +22,15 @@ session_start();
     <script>
     </script>
 <?php
+//checks to see if the username has been set
+//if username is set then the user variable is set to the be posted username variable from the login form
 if(isset($_POST['username']))
-{
-	
-	$user = $_POST['username'];
+{	$user = $_POST['username']; //sets the user variable to be the value stored in the post array
 
 	if($user == $_POST['username'])
 	{
 		$_SESSION['username'] = $user;
+	//prints out the entire navigation bar.
         echo "
             <div class='container-fluid'>
         <nav class='navbar navbar-default'>
@@ -54,7 +53,7 @@ if(isset($_POST['username']))
           </div><!-- /.container-fluid -->
         </nav>
     </div>";
-
+//establishes the connection to the database
 $DBserver = "csmysql.cs.cf.ac.uk"; //mysql server
 $DBuser = "group6.2015"; //mysql username
 $DBpass = "bhF54FWzyq"; //mysql password
@@ -64,29 +63,20 @@ if( $db === FALSE ){
 header( "Location: error.html" ); //redirects to an error page in case of an error.
 die();
 }
-
-function setModule($Mod){
-$query = 'SELECT * FROM MODULE WHERE Module_ID="'.$Mod.'"';
-$result = mysqli_query($db, $query);
-while ($row = mysqli_fetch_assoc($result)){
-	$moduleID = $row['Module_ID'];
-	$moduleTitle = $row['Module_TITLE'];
-	$moduleDesc = $row['Module_DESCRIPTION'];
-	$moduleYear = $row['Module_YEAR'];
-
-	}
-}
+//query retrieves the module id from student takes module where the student id is the same as the value stored for username in the post array.
+//this checks if a student takes a certain module, and only retrieves the modules that the student is enrolled on.
 $query = 'SELECT Module_ID FROM STUDENT_TAKES_MODULE WHERE Student_ID="'.$_POST["username"].'"';
 $result = mysqli_query($db, $query) or die(mysqli_error($db));
 while ($row1 = mysqli_fetch_assoc($result)){
 $moduleID = $row1['Module_ID'];
+$moduleTitle = $row1['Module_TITLE'];
 
 echo"<div class='row'>
 <form action='module-1.php' method='POST'>
 
 <div class='container'>
     <div class='well modules-buttons'>
-        <input hidden type='text' id='username' name='username' value= ".$_SESSION['username']."><a href='module-1.html?moduleID=".$row1['Module_ID']."'><button class='btn-lg'>".$row1['Module_ID']."</button></a><br>
+        <input hidden type='text' id='username' name='username' value= ".$_SESSION['username']."><a href='module-1.html?moduleID=".$row1['Module_ID']."'><button class='btn-lg'>".$row1['Module_ID'], $row1['ModuleTitle']."</button></a><br>
         <input hidden type='text' id='moduleID' name='moduleID' value= ".$moduleID.">
 </form>
     </div>
@@ -96,10 +86,11 @@ echo"<div class='row'>
 //$_SESSION['moduleID'] = $moduleID;
 	}
 }
-else{
-	//$user = $_SESSION['username'];
+//checks to see if the username is not set then redirects users to the login page
+if(!isset($_POST['username']))
+{
 	echo "<p>no user found.</p>";
-	header('location:login-2.php');
+	header('location:adminMenu.html');
 	}
 ?>
 </body>
