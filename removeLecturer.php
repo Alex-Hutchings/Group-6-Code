607e823f565e96f2e6f6dde8af3386014df1a669
Alex-Hutchings <hutchalex@live.co.uk>
2016-04-13 20:36:44 +0100

    Update removeLecturer.php
    commented

--- removeLecturer.php
+++ removeLecturer.php
@@ -5,11 +5,12 @@
 </head>
 <body>
 <?php
-$server = "csmysql.cs.cf.ac.uk";
-$user = "group6.2015"; 
-$password = "bhF54FWzyq"; 
-$database = "group6_2015";
-$db = mysqli_connect($server,$user,$password,$database); 
+//establishing connection to the database
+$server = "csmysql.cs.cf.ac.uk"; //server connection
+$user = "group6.2015";  //the username of user to access the database
+$password = "bhF54FWzyq";  //password of the user to access the database
+$database = "group6_2015"; //the specific database itself
+$db = mysqli_connect($server,$user,$password,$database);  //connection to the database using the variables created above
 if( $db === FALSE ){
 header( "Location: error.html" ); die();
 }
@@ -17,14 +18,16 @@ $update = null;
 
 if ($db->connect_error) {
 die("Connection failed: " . $db->connect_error); }
-if(isset($_SESSION['id'])){
+if(isset($_SESSION['id'])){ //if an id exists in the session array then the update variable is set to that value in the array.
 $update = $_SESSION['id'];
 }
 // sql command to update a record
+//deletes the required id field from the table lecturer where there is a match between the ID and the lecturer ID in the database
 $id = $_GET['id'];
 $sql = "DELETE FROM LECTURER WHERE Lecturer_ID = $id" ;
 
-
+//if the sql query is successful then a message is displayed
+//if the sql is unsuccessful then a message is displayed
 if ($db->query($sql) === TRUE) {
 $message = "Record deleted successfully"; echo $message;
 } else {
